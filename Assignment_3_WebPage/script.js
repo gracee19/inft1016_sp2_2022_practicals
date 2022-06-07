4d48501e7756a0ada2cca75d6575e9eb254da905
Mary Grace Palenzuela <palmg001@mymail.unisa.edu.au>
2022-06-07 10:56:14 +0930

    javascript added buttons are now working

---
+++ Assignment_3_WebPage/script.js
@@ -0,0 +1,20 @@
+function btnClick(){
+    document.getElementById('image').style.display="block";
+    document.getElementById('myBtn').style.display="none";
+}
+function btnClick2(){
+    document.getElementById('hen').style.display="block";
+    document.getElementById('myBtn2').style.display="none";
+}
+function btnClick3(){
+    document.getElementById('stilt').style.display="block";
+    document.getElementById('myBtn3').style.display="none";
+}
+function btnClick4(){
+    document.getElementById('thornBill').style.display="block";
+    document.getElementById('myBtn4').style.display="none";
+}
+function btnClick5(){
+    document.getElementById('robin').style.display="block";
+    document.getElementById('myBtn5').style.display="none";
+}
